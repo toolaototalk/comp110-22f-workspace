9305f33210129213a2ebff4faf211c0ec0f9d1c0
Tianjun Ma <mosken@ad.unc.edu>
2022-08-27 23:00:05 -0400

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,30 +2,30 @@
 
 __author__ = "730521912"
 
-word_input = input("Enter a 5-character word:")
+word_input = input("Enter a 5-character word: ")
 if len(word_input)!=5:
     print("Error: Word must contain 5 characters")
     exit()
-single_char = input("Enter a single character:")
+single_char = input("Enter a single character: ")
 if len(single_char)!=1 or single_char==" ":
     print("Error: Character must be a single character.")
     exit()
 print("Searching for "+ single_char + " in hello")
 count_instance = 0
 if word_input[0]== single_char:
-    print("found "+single_char+ " at index 0")
+    print(single_char + " found" + " at index 0")
     count_instance+=1
 if word_input[1]== single_char:
-    print("found "+single_char+ " at index 1")
+    print(single_char + " found" + " at index 1")
     count_instance+=1
 if word_input[2]== single_char:
-    print("found "+single_char+ " at index 2")
+    print(single_char + " found" + " at index 2")
     count_instance+=1
 if word_input[3]== single_char:
-    print("found "+single_char+ " at index 3")
+    print(single_char + " found" + " at index 3")
     count_instance+=1
 if word_input[4]== single_char:
-    print("found "+single_char+ " at index 4")
+    print(single_char + " found" + " at index 4")
     count_instance+=1
 if count_instance==0:
     print("No instances of " + single_char+ " found in "+ word_input)
