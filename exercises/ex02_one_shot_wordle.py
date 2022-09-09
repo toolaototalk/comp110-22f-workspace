039a1eaa4db7c5f24e8cb1e5d4d9a656ddb79792
Tianjun Ma <mosken@ad.unc.edu>
2022-09-08 22:40:18 -0400

    Progress in ex2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,8 +1,5 @@
-"""One shot wordle exercise"""
-from string import whitespace
-
-
-_author_ = "730521912"
+"""One shot wordle exercise."""
+__author__ = "730521912"
 
 
 WHITE_BOX: str = "\U00002B1C"
@@ -16,21 +13,19 @@ while len(guess) != len(secret_word):
     guess = input(f"That was not {len(secret_word)} letters! Try again:")
 
 while guessIndex < len(secret_word):
-        if guess[guessIndex] == secret_word[guessIndex]:
-            result = result + GREEN_BOX
-        else:
-            in_the_word = False
-            index = 0
-            while  (not in_the_word) & ( index < len(secret_word) ):
-                in_the_word = guess[guessIndex] == secret_word[index]
-                index += 1
+    if guess[guessIndex] == secret_word[guessIndex]:
+        result = result + GREEN_BOX
+    else:
+        in_the_word = False
+        index = 0
+        while (not in_the_word) & (index < len(secret_word)):
+            in_the_word = guess[guessIndex] == secret_word[index]
+            index = index + 1
             if in_the_word:
-                 result = result + YELLOW_BOX
-            else: 
-                result = result + WHITE_BOX
-        guessIndex += 1 
-
-
+                result = result + YELLOW_BOX
+        if (not in_the_word):
+            result = result + WHITE_BOX
+    guessIndex = guessIndex + 1 
 
 if guess == secret_word:
     print(result + "\n" + "Woo! You got it!")
