faa748c4e67602f3a5a05cdd1cdbef66930abcac
Tianjun Ma <mosken@ad.unc.edu>
2022-09-18 20:20:37 -0400

    Progress on ex

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,19 +1,21 @@
-""" List Utility Functions."""
+"""List Utility Functions."""
 __author__ = "730521912"
 
-# def all(int_list: list[int], single_int: int) -> bool:
-#     '''Return if the all the elements in int_list is the same as single_int.'''
-#     i = 0
-#     if len(int_list) == 0:
-#         return False
-#     while i < len(int_list):
-#         if int_list[i] != single_int:
-#             return False
-#         i += 1
-#     return True
+
+def all(int_list: list[int], single_int: int) -> bool:
+    """Return if the all the elements in int_list is the same as single_int."""
+    i = 0
+    if len(int_list) == 0:
+        return False
+    while i < len(int_list):
+        if int_list[i] != single_int:
+            return False
+        i += 1
+    return True
+
 
 def max(input: list[int]) -> int:
-    '''Return the largest int in the input list.'''
+    """Return the largest int in the input list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     i = 1
@@ -21,22 +23,17 @@ def max(input: list[int]) -> int:
     while i < len(input):
         if input[i] > max_value:
             max_value = input[i]
-            i = i + 1
-            print(i)
+        i = i + 1
     return max_value
 
 
-print(max([1]))
-
-# def is_equal(list1: list[int], list2: list[int]) -> bool:
-#     """Return if two input lists are deeply equal to one another"""
-#     i = 0
-#     if len(list1) != len(list2):
-#         return False
-#     while i < len(list1):
-#         if list1[i] != list2[i]:
-#             return False
-#         i += 1
-#     return True
-
-
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Return if two input lists are deeply equal to one another."""
+    i = 0
+    if len(list1) != len(list2):
+        return False
+    while i < len(list1):
+        if list1[i] != list2[i]:
+            return False
+        i += 1
+    return True
